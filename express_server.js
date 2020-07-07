eda672e687f0f05922d55fba6c9c227a05f1db5b
Simon Jung <simon.hb.jung@hotmail.com>
2020-07-07 00:40:51 +0000

    added URL submission form where you can post, created random string generator

--- express_server.js
+++ express_server.js
@@ -1,6 +1,18 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
+const bodyParser = require("body-parser");
+
+function generateRandomString() {
+  let randomString;
+  for (let i = 1; i <= 6; i++) {
+    let r = Math.random().toString(36).substring(7)
+    randomString += r;
+  }
+  return randomString;
+}
+
+app.use(bodyParser.urlencoded({extended: true}))
 
 app.set("view engine", "ejs");
 
@@ -39,7 +51,16 @@ app.get("/set", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls/:shortURL", (req, res) => {
   let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars);
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
 });
