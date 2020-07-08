e1bd4a55314015ee651a03039479f4400641a12a
Simon Jung <simon.hb.jung@hotmail.com>
2020-07-08 00:18:23 +0000

    logout button now clears cookie and redirects to /urls

--- express_server.js
+++ express_server.js
@@ -95,4 +95,9 @@ app.post("/login", (req, res) => {
   res.redirect('/urls')
 });
 
+app.post("/logout", (req, res) => {
+  res.clearCookie('username');
+  res.redirect('/urls')
+});
+
 app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`));
