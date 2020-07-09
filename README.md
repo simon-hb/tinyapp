5c4441fed46a69e14d5db3c19c88c388a286df95
Simon Jung <simon.hb.jung@hotmail.com>
2020-07-09 23:05:10 +0000

    refactored html pages, added more tests, added readme and screenshots

---
+++ README.md
@@ -0,0 +1,26 @@
+# TinyApp Project
+
+TinyApp is a full stack web application built with Node and Express that allows users to shorten URLs into a condensed link of 6 random characters. This is my first application I have ever created. More to come!
+
+## Final Product
+
+!["screenshot description"](#)
+!["screenshot description"](#)
+
+## Dependencies
+
+- Node.js
+- Express
+- EJS
+- bcrypt
+- body-parser
+- cookie-session
+
+## Getting Started
+
+- Install all dependencies (using the `npm install` command).
+- Run the development web server using the `node express_server.js` command.
+- Open http://localhost:8080/ on your browser to get started
+
+### Project Description
+My first web application does a lot of cool things! You can register, logout, and login. The cookies and database used to store log-in data are all encrypted or hashed so it is perfectly safe! Once logged in, you can create shortened URL's and use them the same as the long version. You can also see everything you created in a table and edit/ delete. Please navigate around and see for yourself!
--- test/helpersTest.js
+++ test/helpersTest.js
@@ -1,6 +1,6 @@
 const { assert } = require('chai');
 
-const { getUserByEmail } = require('../helpers.js');
+const { getUserByEmail, generateRandomString, urlsForUser, urlDatabase } = require('../helpers.js');
 
 const users = {
   "userRandomID": {
@@ -26,5 +26,33 @@ describe('getUserByEmail', function() {
     const user = getUserByEmail("haha@example.com", users);
     const expectedOutput = undefined;
     assert.strictEqual(user, expectedOutput);
+  });
+});
+
+describe('generateRandomString', function() {
+  it('should be 6 characters long', function() {
+    const randomString = generateRandomString();
+    const expectedOutput = 6;
+    assert.strictEqual(randomString.length, expectedOutput);
+  });
+
+  it('should return a string value', function() {
+    const randomString = generateRandomString();
+    const expectedOutput = 'string';
+    assert.strictEqual(typeof randomString, expectedOutput);
+  });
+});
+
+describe('urlsForUser', function() {
+  it('should return all the key-value pairs that belong to particular id from urlDatabase', function() {
+    const urlForID = urlsForUser('a');
+    const expectedOutput = urlDatabase;
+    assert.deepEqual(urlForID, expectedOutput);
+  });
+
+  it('should return empty object if no shortURLS belong to the id', function() {
+    const urlForID = urlsForUser('hello!');
+    const expectedOutput = {};
+    assert.deepEqual(urlForID, expectedOutput);
   });
 });
